0a579f1f01a2d74d40c644ba4e304c31b4838236
Poorvi Dharwad <poorvid@cybage.com>
2020-06-15 15:24:05 +0530

    commit-1, junit , maven

---
+++ src/test/java/JunitProject_2/JunitProject_2/FirstTest.java
@@ -0,0 +1,44 @@
+package JunitProject_2.JunitProject_2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTest {
+	
+WebDriver driver;	
+	
+	@Test
+	public void ValidLoginTest()
+	{
+		System.setProperty("webdriver.gecko.driver", "lib/geckodriver.exe");
+	    WebDriver driver = new FirefoxDriver();
+	    
+		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		
+		driver.quit();
+		
+	}
+	
+	@Test
+	public void CurrentURL()
+	{
+		System.setProperty("webdriver.gecko.driver", "lib/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+	    
+	    driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+	   
+	    String currentURL=driver.getCurrentUrl();
+	    Assert.assertTrue(currentURL.contains("index.php/auth/login1"));
+	    
+	  
+		
+		
+	}
+
+}
